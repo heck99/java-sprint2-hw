c69679da1c71eccd67cf9771b76310fef0ea32e9
heck <ramazan280699@gmail.com>
2022-02-20 19:27:46 +0300

    feat: добавить создание задачи из строки

--- src/Functional/FileBackedTasksManager.java
+++ src/Functional/FileBackedTasksManager.java
@@ -1,6 +1,6 @@
 package Functional;
 
-import allTasks.Task;
+import allTasks.*;
 
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -8,6 +8,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileBackedTasksManager extends InMemoryTasksManager{
     String path;
@@ -25,12 +27,18 @@ public class FileBackedTasksManager extends InMemoryTasksManager{
         //Всегда ли нужно указывать кодировку при создании FileWriter/FileReader
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
             bw.write(String.join(",","id", "type", "name", "status", "description", "epic"));
-            for (Integer taskId : tasksMap.keySet()) {
+            for (Task task :this.getTaskList()) {
                 bw.newLine();
-                bw.write(tasksMap.get(taskId).toString(1));
+                bw.write(task.toString(1));
             }
             bw.newLine();
-            bw.write(historyManager.toString());
+            String str = "";
+            for (Task task : history()) {
+                str += task.getId() + ",";
+            }
+            if(!str.isEmpty()) {
+                bw.write(str.substring(0, str.length() - 1));
+            }
         } catch (IOException ex) {
 
         }
@@ -66,5 +74,27 @@ public class FileBackedTasksManager extends InMemoryTasksManager{
     public void deleteTask(int id) {
         super.deleteTask(id);
         save();
+    }
+    private Task fromString(String value) {
+        String[] fields = value.split(",");
+        Task taskToReturn;
+        switch (TaskType.valueOf(fields[1])) {
+            case TASK:
+                taskToReturn = new Task(fields[2], fields[4], Status.valueOf(fields[3]));
+                taskToReturn.setId(Integer.parseInt(fields[0]));
+                break;
+            case EPIC:
+                taskToReturn = new EpicTask(fields[2], fields[4]);
+                taskToReturn.setId(Integer.parseInt(fields[0]));
+                break;
+            case SUBTASK:
+                taskToReturn = new SubTask(fields[2], fields[4], Status.valueOf(fields[3]),
+                        this.getEpicById(Integer.parseInt(fields[5])));
+                taskToReturn.setId(Integer.parseInt(fields[0]));
+                break;
+            default:
+                return null;
+        }
+        return taskToReturn;
     }
 }
